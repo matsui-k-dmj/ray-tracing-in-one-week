da3c86d434362a5688cf2915894fda38395d4955
matsui-k <osskil611@gmail.com>
2022-11-13 01:38:28 +0900

    Rayクラス

---
+++ ray.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "vec3.h"
+
+class Ray {
+public:
+	Point3 m_origin{};
+	Vec3 m_direction{};
+
+	Ray() = default;
+
+	Ray(const Point3& origin, const Vec3& direction)
+		: m_origin{ origin }, m_direction{ direction } {}
+
+	auto at(double t) const -> Point3 {
+		return m_origin + t * m_direction;
+	}
+};
