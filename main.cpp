41434f09bcac7eb029620ce88f58ccef613a5515
matsui-k <osskil611@gmail.com>
2022-11-17 02:34:02 +0900

    Metal 2個置いた

--- main.cpp
+++ main.cpp
@@ -64,13 +64,15 @@ int main() {
 	constexpr int max_refletion = 10;
 
 	auto ground_material = std::make_shared<Lambertian>(Color{ 0, 0.1, 0.1 });
-	auto matt_material = std::make_shared<Lambertian>(Color{ 0.9, 0.9, 0.9 });
+	auto matt_material = std::make_shared<Lambertian>(Color{ 0.98, 0.51, 0.48 });
 	auto metal_material = std::make_shared<Metal>(Color{ 0.8, 0.8, 0.8 });
+	auto metal_perfect_material = std::make_shared<Metal>(Color{ 1.0, 1.0, 1.0 });
+
 
 	HittableList world{};
-	world.add(std::make_shared<Sphere>(Point3{ 0, 0, -1 }, 0.5, matt_material));
-	world.add(std::make_shared<Sphere>(Point3{ 1, 0, -1 }, 0.5, metal_material));
-	world.add(std::make_shared<Sphere>(Point3{ -1, 0, -2 }, 0.5, metal_material));
+	world.add(std::make_shared<Sphere>(Point3{ 0, 0, -1.5 }, 0.5, matt_material));
+	world.add(std::make_shared<Sphere>(Point3{ 1, 0, -1.0 }, 0.5, metal_material));
+	world.add(std::make_shared<Sphere>(Point3{ -0.7, 0, -0.5 }, 0.5, metal_perfect_material));
 
 	world.add(std::make_shared<Sphere>(Point3{ 0, -100.5, -1 }, 100.0, ground_material)); // 地面
 
